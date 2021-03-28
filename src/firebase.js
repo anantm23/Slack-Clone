1e9356e4fe7f4d4655626bc17a0c06e28c5b6d85
anantm23 <57768228+anantm23@users.noreply.github.com>
2021-03-28 09:21:53 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB2RXzWd2PKT3vo1EwxhSKGiF8gagpIKuU",
-    authDomain: "slack-clone-804b6.firebaseapp.com",
-    projectId: "slack-clone-804b6",
-    storageBucket: "slack-clone-804b6.appspot.com",
-    messagingSenderId: "953295365008",
-    appId: "1:953295365008:web:cbe7ad78bb22a6eaca4915"
-  };
+     };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
